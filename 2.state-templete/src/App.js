10dac1100647c5b02a44633e2370bd4b6c2c28c6
roeuihyun <roeuihyun@gmail.com>
2022-09-10 01:49:04 +0900

    4-5강 state 적용 실습(4/4)
    리액트 + 몹엑스 기초 강의 - 4-5강 state 적용 실습(4/4)

--- 2.state-templete/src/App.js
+++ 2.state-templete/src/App.js
@@ -3,15 +3,30 @@ import { Container, Grid } from '@material-ui/core';
 import SearchBar from './components/SearchBar';
 import BookList from './components/BookList';
 import Books from './static_data/Books';
+import BookDetail from './components/BookDetail';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      books : Books,
+      selectedBook : Books[0],
+    }
+  }
+
   render(){
     return(
       <Container>
         <SearchBar />
         <Grid container spacing = { 2 } >
           <Grid item>
-            <BookList books = {Books} />
+            <BookList books = { this.state.books } />
+          </Grid>
+          <Grid item>
+            <BookDetail book = { this.state.selectedBook }>
+
+            </BookDetail>
           </Grid>
         </Grid>
       </Container>
