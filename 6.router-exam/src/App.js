e25b310befa987f12b5bc420227eb98017bb1db1
roeuihyun <roeuihyun@gmail.com>
2022-09-11 01:43:53 +0900

    7-2강 React-Router 실습(1)
    리액트 + 몹엑스 기초 강의 - 7-2강 React-Router 실습(1)

--- 6.router-exam/src/App.js
+++ 6.router-exam/src/App.js
@@ -4,15 +4,19 @@ import Nav from './components/Nav';
 import About from './components/About'
 import Main from './components/Main'
 import Lessons from './components/Lessons'
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Nav />
-        <Main />
-        <About />
-        <Lessons />
+        <Switch>
+          <Route exact path='/' component={Main}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/lessons' component={Lessons}></Route>
+          <Route component={ () => <h2>Not Found!</h2>}></Route>
+        </Switch>
       </div>
     );
   } 
--- 6.router-exam/src/components/Nav.js
+++ 6.router-exam/src/components/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link, NavLink } from 'react-router-dom';
 
 class Nav extends Component {
   render(){
@@ -6,9 +7,15 @@ class Nav extends Component {
       <nav className='navtop'>
         <h2>Namoosori</h2>
         <ul className='nav-links'>
-          <li>Main</li>
-          <li>About</li>
-          <li>Lessons</li>
+          {/* <li><a href='/'>Main</a></li>
+          <li><a href='/about'>About</a></li>
+          <li><a href='/lessons'>Lessons</a></li> */}
+          {/* <li><Link to='/'>Main</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/lessons'>Lessons</Link></li> */}
+          <li><NavLink exact to='/'>Main</NavLink></li>
+          <li><NavLink exact to='/about'>About</NavLink></li>
+          <li><NavLink exact to='/lessons'>Lessons</NavLink></li>
         </ul>
       </nav>
     )
